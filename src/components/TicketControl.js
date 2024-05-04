7018f622a8b1c604b190122e9ca8d21a97bca92c
kwesi <kwesi.makonnen@gmail.com>
2024-05-04 19:52:29 +0300

    Buttin added for ticket control, cant toggle back

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -11,18 +11,27 @@ class TicketControl extends React.Component {
         };
     }
 
+    handleClick = () => {
+        this.setState(prevState => ({  // This function uses parentheses () around the object returned by the arrow function. This syntax indicates an implicit return, meaning that the object inside the parentheses is the return value of the arrow function.
+            formVisibleOnPage: !prevState.formVisibleOnPage
+        }))
+    }
+
     render() {
         let currentVisibleState= null;
+        let addTicketbutton = null;
 
         if(this.state.formVisibleOnPage){
             currentVisibleState = <NewTicketForm/>
         } else {
             currentVisibleState = <TicketList/>
+            addTicketbutton= <button onClick={this.handleClick}>Add Ticket</button>
         }
 
         return (
             <React.Fragment>
                 {currentVisibleState}
+                {addTicketbutton}
             </React.Fragment>
         );
     }
