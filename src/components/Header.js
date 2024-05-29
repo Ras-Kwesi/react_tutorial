e67f42c3bf3978cc94642515507f724267ef7f85
kwesi <kwesi.makonnen@gmail.com>
2024-05-29 22:29:43 +0300

    styles added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ticketsImage from './../img/tickets-image-c1addb252b0e58560cb15ffa2b317560.png';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const HelpQueueHeader = styled.h1`
+    font-size: 24px;
+    text-align: center;
+    color: white;
+    background-color: purple;
+`;
 
 function Header() {
     return (
         <React.Fragment>
-            {/* <h1> Help Queue</h1> */}
+            <HelpQueueHeader>
+                Help Queue
+            </HelpQueueHeader>
             <img src={ticketsImage} alt='Tickets' />
             <ul>
                 <li>
