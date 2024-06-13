ce7dd7c6170751a528c76672a6d9c9da8da8fe9c
kwesi <kwesi.makonnen@gmail.com>
2024-06-14 00:10:30 +0300

    Themes added to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,56 @@
-import React from "react";
-import Header from './Header'
+import React, { useState } from "react";
+import Header from "./Header";
 import TicketControl from "./TicketControl";
-
-
+import ToggleTheme from "./ToggleTheme";
+import { ThemeContext, themes } from "../context/theme-context";
 
 function App() {
-    const name = "Thato";
-    const name2 = "Haley";
+    const [theme, setTheme] = useState(themes.grey);
+
+    document.body.style.backgroundColor = theme.backgroundColor; // new code!
+    document.body.style.color = theme.textColor; // new code!
+
+    // function toggleTheme() {
+        // setTheme(theme =>
+        //     theme.textColor === "AntiqueWhite" ? themes.light : themes.dark
+        // );
+        // setTheme(theme => {
+        //     if (theme.textColor === "Black") {
+        //         return themes.light
+        //     } else if (theme.textColor === "DarkSlateGrey") {
+        //         return themes.grey
+        //     } else if (theme.textColor === "AntiqueWhite") {
+        //         return themes.dark
+        //     }
+        //     // else {
+        //     //     return themes.dark
+        //     // }
+        // });
+
+    // }
+
+    function toggleTheme() {
+        setTheme(currentTheme => {  // It swithces since each case holds the other theme. So white holds grey, grey black and black white
+            switch (currentTheme) {
+                case themes.light:
+                    return themes.grey;
+                case themes.grey:
+                    return themes.dark;
+                case themes.dark:
+                    return themes.light;
+                default:
+                    return themes.light;
+            }
+        });
+    }
+
     return (
-        <React.Fragment>
-            <Header/>
+        <ThemeContext.Provider value={theme}>
+            <Header />
             <hr />
-            <TicketControl/>
-        </React.Fragment>
+            <ToggleTheme toggleTheme={toggleTheme}/>
+            <TicketControl />
+        </ThemeContext.Provider>
     );
 }
 
